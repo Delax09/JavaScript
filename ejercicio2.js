b488b0f6713985fd5d2b9dc60135565740792acb
Delax09 <98916973+Delax09@users.noreply.github.com>
2022-12-06 16:32:30 -0300

    Add files via upload

---
+++ ejercicio2.js
@@ -0,0 +1,14 @@
+// ejercicio 2
+
+var nombre = "Fabian"
+var edad = 20
+var trabajo = true
+var fecha_nac = new Date(2002, 02,08)
+
+const libro = {
+    titulo: "Cancion de hielo y fuego 1: Juego de Tronos",
+    autor: "George R.R Martin",
+    fecha: new Date(2014,11,2)
+}
+console.log(libro)
+
