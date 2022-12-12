75f0cd00be115a8cc991b7d4822a77b760ddc664
Delax09 <98916973+Delax09@users.noreply.github.com>
2022-12-12 15:27:52 -0300

    Add files via upload

---
+++ ejercicios_number.js
@@ -0,0 +1,11 @@
+var altura_cm = 175
+var altura_m = 1.75
+var masa_kg = 80.1
+
+var altura_redon = altura_m.toFixed(1)
+
+var masa_redon = masa_kg.toPrecision(4)
+
+var max = Number.MAX_VALUE +1 
+
+console.log(max)
