11036d044bb43033f9f16ea80fa97c4246d17991
Delax09 <98916973+Delax09@users.noreply.github.com>
2022-12-12 08:27:22 -0300

    Add files via upload

---
+++ ejercicios_strings.js
@@ -0,0 +1,17 @@
+// ejercicio_strings.js
+
+var nombre = "fabian"
+var apellido = "mejias"
+
+console.log(`${nombre} ${apellido}`)
+console.log(nombre.toLowerCase()+ " "+ apellido.toLowerCase())
+console.log(nombre.toUpperCase()+ " "+ apellido.toUpperCase())
+
+var estudiante = "e s t u d i a n t e"
+
+var pri_let_nom = nombre[0]
+var ult_let_ape = apellido[6]
+
+console.log(estudiante.replace(/ /g, ""))
+
+var estudiante = false
