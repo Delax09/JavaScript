21bc3b0ca1aceb0740fb96094fc42b054ae4c8b4
Delax09 <98916973+Delax09@users.noreply.github.com>
2022-12-22 20:48:27 -0300

    Add files via upload

---
+++ ejercicio_set.js
@@ -0,0 +1,14 @@
+var lista_familia = ["Fabian", "Fabiana", "Farid", "Carline"]
+
+console.log(lista_familia)
+
+lista_familia.push("Fabian")
+
+console.log(lista_familia)
+
+var set_familia = new Set(lista_familia)
+console.log(set_familia)
+
+set_familia.add("JavaScript")
+
+console.log(set_familia)
