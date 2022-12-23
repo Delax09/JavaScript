2fbda5f254f55d69048b88d99d64905fa5394403
Delax09 <98916973+Delax09@users.noreply.github.com>
2022-12-22 21:20:02 -0300

    Add files via upload

---
+++ objeto.js
@@ -0,0 +1,27 @@
+// objeto.js
+
+var objeto = {
+    nombre: "Fabian",
+    apellido: "Mejias",
+    edad: 20,
+    desarrollador: true
+}
+
+var var_edad = "edad"
+
+console.log(objeto[var_edad])
+
+var lista_datos = [objeto.nombre, objeto.apellido, objeto.edad, objeto.desarrollador]
+
+var mj1 = {
+    nombre: "Sophia",
+    apellido: "Alarcon", 
+    edad: 19
+}
+
+var mj2 = {
+    nombre: "Efrain",
+    apellido: "Gomez",
+    edad: 19
+}
+
